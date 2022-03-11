cfc98464a23bf3fb6e443542f257ce217fb16f82
Rafael Dias <rafael.crd98@gmail.com>
2022-03-10 22:23:28 -0300

    feat: tic tac toe

--- aula1/src/App.java
+++ aula1/src/App.java
@@ -1,34 +1,91 @@
+import java.util.Scanner;
+
 public class App {
 
-    int cardsPlayer1 = 0;
-    static int maxCards = 54;
-
-    static int[] cardsPlayer2 = {0,3,4,5,6};
-
-    static int [] cards = new int[maxCards];
-
-    static int [][] cartasCorNumeroMatrix = new int [25][54];
-    int [][] cartasCorNumeroMatrix2 = {
-        {40,73,49,5,96},
-        {10,3,4,5,6},
-        {0,32,44,55,66},
-        {6,8,0,15,61}
-
-    };
-    
+    static int [][] ticTacToeTable = new int [3][3];
+    static int turn = 0;
  
     public static void main(String[] args) throws Exception {
-        
-        for(int i=0; i< 25; i++){
-            for(int j=0; j< 54; j++){
-                System.out.println(cartasCorNumeroMatrix[i][j]);
-            }
+
+        // vamos limpar o tabuleiro colocando um "-1"
+        initTable();
+        Scanner scanner = new Scanner(System.in);
+
+        while (!isGameOver()){
+            int player = getPlayerTurn(turn);
+            System.out.println(String.format("Jogador %d - Digite o número da casa", player));
+            printTable();
+            System.out.print("Digite sua jogada: ");
+            int playerMove = scanner.nextInt();
+            System.out.println(isValidPosition(playerMove));
+            turn++; 
+            break;   
         }
 
+        
+        
+        
 
 
 
+        scanner.close();
 
+    }
 
+    public static void initTable(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                ticTacToeTable[i][j] = -1;
+            }
+        }
+    }
+
+    public static int getPlayerTurn(int turn){
+        //retorna quem irá jogar
+        //jogador 1 ou jogador 2
+        if (turn %2 == 0){
+            return 1;
+        }else{
+            return 2;
+        }
+
+    }
+
+    public static boolean isGameOver(){
+        return false;
+    }
+
+    public static void printTable(){
+
+        for(int i=0;i<3;i++){
+            String value1 = getTableValue(i,0);
+            String value2 = getTableValue(i,1);
+            String value3 = getTableValue(i,2);
+            String line = String.format("%s %s %s", value1, value2, value3);
+            System.out.println(line);
+        }
+        
+    }
+
+    public static String getTableValue(int posX, int posY){
+        int positionValue = ticTacToeTable[posX][posY];
+        if (positionValue == -1){
+            return ""+(posX*3+posY);
+        }else if (positionValue == 1){
+            return "x";
+        }
+        return "o";
+    }
+
+    public static boolean isValidPosition(int position){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if (i*3 + j == position){
+                    int positionValue = ticTacToeTable[i][j];
+                    return positionValue == -1;
+                }
+            }
+        }
+        return false;
     }
 }
